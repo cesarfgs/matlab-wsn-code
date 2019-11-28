f0777912212e89c35aae8eced7e129784ff3bff5
cesar felipe gonçalves silva <cesar@cesarfelipe.com.br>
2019-11-28 16:57:57 -0300

    Update main.m
    1) you can choose node number (nodeNo parameter)
    2) you can choose node signal range (range parameter - Its a 1x1 Kilometer area, algo range in meters)
    3) once network nodes position randomly formed and all nodes neighbors by radius (range) criteria...
    3.1) Shorthestpath nodes discovered between node 1 (sender) and node 2 (receiver) start energy consumption due routing (forward) task...
    3.2) As soon 1 node dies, current route becames "useless"
    3.3) New shortest path is discovered... (restart 3.1 till no more routes available)
    4) I assume that every energy consumption loop is 1 packet transmittion
    5) At the end, it "reports":
    5.1) All routes discovery events from the begging till the end of network (no more routes)
    5.2) hop count of each route iteration
    5.3) Nodes involved (In the same order they are met in route)
    5.4) Hypothetical packets transmitted
    5.5) Dead node number (ID) every iteration
    6) It also plots the original WSN
    7) ALso plot every routing loop result and report
    8) Mark all nodes involved ini each route event
    9) Mark all dead nodes
    10) Write reports to an external file into same folder the main.m file is
    
    I hope it helps some one....

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ clear all
 close all
 clc
 
-%% GPU config
+%% GPU config - if want to run some code block into GPU. (NOT FULLY IMPLEMENTED)
 %gpu = gpuDevice;
 %gpu(1);
 
@@ -19,8 +19,10 @@ dataset.minEnergy = 80; % Mw - Miliwatts (70% energy)
 dataset.maxEnergy = 100; % Mw - Miliwatts (Full energy (100%) - 1 mAh charge capacity within 1 Volt energy)
 dataset.energyconsumptionperCicle = 0.35;
 dataset.energyrecoveryperCicle = 0.2;
+dataset.energyfactor = 0.001;
 STenergy=10000;
 packet=0;
+iterationcounter=1;
 
 % Node position sortition
 
@@ -92,8 +94,6 @@ for a = 1 : height(G.Edges)
 	G.Edges.SourcenodeYpos(a) = dataset.nodePosition(garbage.Sourcenode,3);
 	G.Edges.TargetnodeXpos(a) = dataset.nodePosition(garbage.Targetnode,2);
 	G.Edges.TargetnodeYpos(a) = dataset.nodePosition(garbage.Targetnode,3);
-    %G.Edges.SourcenodeEnergy(a) = dataset.nodePosition(garbage.Sourcenode,4);
-    %G.Edges.TargetnodeEnergy(a) = dataset.nodePosition(garbage.Targetnode,4);
     G.Edges.ActiveEdge(a) = 1;
 end
 
@@ -105,14 +105,19 @@ garbage.Xmax = 1500;
 garbage.Xmin = 0;
 garbage.Ymax = 1500;
 garbage.Ymin = 0;
-p = plot(G,'XData',(dataset.nodePosition(:,2)),'YData',(dataset.nodePosition(:,3))); %,'EdgeLabel',G.Edges.Weight
-line(dataset.nodePosition(1:2,2),dataset.nodePosition(1:2,3),'color','green','marker','o','linestyle','none','markersize',100)
+p = plot(G,'XData',(dataset.nodePosition(:,2)),'YData',(dataset.nodePosition(:,3))); 
+line(dataset.nodePosition(1:2,2),dataset.nodePosition(1:2,3),'color','green','marker','o','linestyle','none','markersize',50)
 garbage.ax = gca;
 garbage.ax.XAxis.TickValues = 0:100:1000;
 garbage.ax.YAxis.TickValues = 0:100:1000;
 grid on
 hold on
+title(['Original WSN | ','Nodes number: ',num2str(dataset.nodeNo),' | Nodes range: ', num2str(dataset.range)])
 pause(2)
+
+%Creation of some stuff needed
+garbage.deadnodelist=[];
+garbage.deadnodeneighbors=[];
 
 %% finding routes
 
@@ -122,9 +127,12 @@ G2 = shortestpathtree(G,1,2);
 
 %% Initialize patch existance test for loops
 
+fileID = fopen('report-noACO-simulation.txt','w'); %Open file to collect data to external file
+fprintf(fileID,'%6s %20s %20s %20s %20s\r\n','|NodeNo|','|No ACO Scene|','|Hops|','|Packets sent|','|Dead node|');
+
 while ~isempty(G2.Edges)
     G2 = shortestpathtree(G,1,2);
-    
+    iterationcounter=iterationcounter+1;
     % Test if there is connection between node 1 and 2. If not, terminate!
     if isempty(G2.Edges)
         break
@@ -139,17 +147,34 @@ while ~isempty(G2.Edges)
 
     %Find nodes involved in routing event
     garbage.routingnodes = unique(G2.Edges.EndNodes);
-
+    garbage.routepath = shortestpath(G,1,2);
+            
+    %Construct localization dataset to nodes involved in routing event for plot effects
+    for a = 1 : length(garbage.routingnodes)
+        garbage.b=garbage.routingnodes(a,1);
+        dataset.routingnodesPosition(a,:)=dataset.nodePosition(garbage.b,:);
+    end
+        
+    %Find nodes not involved in routing to energy increase
+    [garbage.Outroutenodes, garbage.Outroutenodesidx] = setdiff(dataset.nodePosition(:,1),garbage.routingnodes(:,1),'stable');
+    
     %Code block for energy usage (decrease) and packet send count
-
     while min(dataset.nodePosition(:,4))>0 
         for a = 1 : length(garbage.routingnodes)
             node=garbage.routingnodes(a,1);
             dataset.nodePosition(node,4)=dataset.nodePosition(node,4)-dataset.energyconsumptionperCicle^rand()+dataset.energyrecoveryperCicle^rand();
-            packet=packet+1
+            packet=packet+1;
+               
         end
+        
+%         for c = 1 : length(garbage.Outroutenodesidx)
+%             rechargeblenode=garbage.Outroutenodesidx(c,1);
+%             dataset.nodePosition(rechargeblenode,4)=dataset.nodePosition(rechargeblenode,4)+dataset.energyrecoveryperCicle*dataset.energyfactor*rand();
+%             b=1;
+%         end
+%line(dataset.nodePosition(1:2,2),dataset.nodePosition(1:2,3),'color','green','marker','o','linestyle','none','markersize',100)
     end
-
+    
     %Find dead node ID to discorver its neighbors and disable relative edges 
     [garbage.deadnoderow] = find(dataset.nodePosition(:,4)<=0);
     for a = 1 : length(garbage.deadnoderow)
@@ -157,13 +182,13 @@ while ~isempty(G2.Edges)
         for b = 1 : height(G.Edges)
             if ismember(G.Edges.EndNodes(b,1),deadnode) == 1 || ismember(G.Edges.EndNodes(b,2),deadnode) == 1
                 G.Edges.ActiveEdge(b)=0;
-                deadnode
+                deadnode;
                 pause(2)
             end
         end
     end
+    garbage.deadnodelist(length(garbage.deadnodelist)+1,1)=deadnode;
     [garbage.deadedgerow]=find(G.Edges.ActiveEdge==0);
-    G = rmedge(G,garbage.deadedgerow(:,1));
 
     %Mark node energy as NaN
     [garbage.deaddatasetnoderow]=find(dataset.nodePosition(:,4)<=0);
@@ -171,12 +196,66 @@ while ~isempty(G2.Edges)
         b=garbage.deaddatasetnoderow(a,1);
         dataset.nodePosition(b,4)=NaN;
     end
+    
+    %Get number of hops between source and destination for this routing
+    %session
+    hopsnumber=length(garbage.routingnodes);
 
+    %Informative section in command line - If need to disable plot loop to increase
+    %processing speed
+    
+    msg=['Without ACO - Scene #: ',num2str(iterationcounter),' | Hops : ',num2str(hopsnumber),' | Packets sent: ', num2str(packet),' | Dead node: ', num2str(deadnode),' | Routing nodes: ', num2str(garbage.routepath)];
+    disp(msg)
+    
+    %plot for every dead edges iteration's result
+    figure('units','normalized','innerposition',[0 0 1 1],'MenuBar','none')
+    p = plot(G,'XData',(dataset.nodePosition(:,2)),'YData',(dataset.nodePosition(:,3))); 
+    line(dataset.nodePosition(1:2,2),dataset.nodePosition(1:2,3),'color','green','marker','o','linestyle','none','markersize',50)
+    garbage.ax = gca;
+    garbage.ax.XAxis.TickValues = 0:100:1000;
+    garbage.ax.YAxis.TickValues = 0:100:1000;
+    hold on
+    
+    %Plot all dead nodes in red
+    for a = 1 : length(garbage.deadnodelist)
+        garbage.b=garbage.deadnodelist(a,1);
+        scatter(dataset.nodePosition(garbage.b,2),dataset.nodePosition(garbage.b,3),'MarkerFaceColor','red');
+    end
+    
+    %Title for every iteration's result
+    title(['WSN shortest path: ',num2str(iterationcounter),' |hops: ',num2str(hopsnumber),' |Packets sent: ',num2str(packet),' |Dead node: ',num2str(deadnode),' |Router nodes: ', num2str(garbage.routepath)])
+    grid on
+    pause(2)
    
-    figure;p = plot(G,'XData',(dataset.nodePosition(:,2)),'YData',(dataset.nodePosition(:,3))); %,'EdgeLabel',G.Edges.Weight
-    pause(2)   
+    %Colect data to append to a external file for later usage - file open
+    %command line in the begining
 
+    report1=[dataset.nodeNo;iterationcounter;hopsnumber;packet;deadnode];
+    fprintf(fileID,'%6.0f %20.0f %20.0f %20.0f %20.0f\r\n',report1);
+
+    %Remove dead edges from graph
+    G = rmedge(G,garbage.deadedgerow(:,1));
+
+    %Mark nodes involved in route with green color
+    scatter(dataset.routingnodesPosition(:,2),dataset.routingnodesPosition(:,3),'MarkerFaceColor','green');
+    pause(0.2)        
+
+    %Clear router nodes position to avoid erroneous plot
+    clear dataset.routingnodesPosition
 end
 
+fclose(fileID); %Close external data collector file
+
+%Plot all dead nodes in red
+    for a = 1 : length(garbage.deadnodelist)
+        garbage.b=garbage.deadnodelist(a,1);
+        scatter(dataset.nodePosition(garbage.b,2),dataset.nodePosition(garbage.b,3),'MarkerFaceColor','red');
+    end
+
+%Title for the last plot
+title(['WSN shortest path: ',num2str(iterationcounter),' |hops: ',num2str(hopsnumber),' |Packets sent: ',num2str(packet),' |Dead node: ',num2str(deadnode),' |Router nodes: ', num2str(garbage.routepath),'{\color{red} - ALL ROUTES UNAVAILABLE}'])
+    
 %Message if there is no path between source and target even in the first iteration
-disp('NO CONNECTIONS BETWEEN SOURCE (NODE1) AND TARGET (NODE2)')
+disp('NO ROUTES BETWEEN SOURCE (NODE1) AND TARGET (NODE2)')
+
+%% ANOTAÇÕES IMPORTANTES
